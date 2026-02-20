bd3acea8278d0f32d7ad2b09629125b76a65a495
niczgalvao <nicolas.galvao.oliveira@escola.pr.gov.br>
2026-02-20 08:52:30 -0300

    Add files via upload

---
+++ atividade1.py
@@ -0,0 +1,16 @@
+### Atividade 1 - Nivelamento ###
+# Autodescrição #
+nome = "Galvão"
+idade = 12
+altura = 1.85
+genero = "masculo"
+cor_de_pele = "pardo"
+cor_de_cabelo = "castanho escuro"
+tamanho_cabelo = "médio"
+estilo_cabelo = "ondulado"
+curso = "desenvolvimento de sistemas "
+hobby_fav = "dormi"
+
+print(f"Salve , meu nome é {nome}, tenho {idade} anos,{altura} de altura, meu genêro é {genero}, sou {pardo}, com a cor de cabelo{cor_de_cabelo}, do tamanho {tamanho_cabelo},e estilo {estilo_cabelo}, estou cursando, {curso} e meu hobby é {hobby_fav}")
+
+
