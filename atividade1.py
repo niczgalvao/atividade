da3f3ddc6cd02b064cfd85e4e4e2343bf68904f5
niczgalvao <nicolas.galvao.oliveira@escola.pr.gov.br>
2026-02-23 09:14:48 -0300

    Fix variable name from 'pardo' to 'cor_de_pele'

--- atividade1.py
+++ atividade1.py
@@ -11,6 +11,6 @@ estilo_cabelo = "ondulado"
 curso = "desenvolvimento de sistemas "
 hobby_fav = "dormi"
 
-print(f"Salve , meu nome é {nome}, tenho {idade} anos,{altura} de altura, meu genêro é {genero}, sou {pardo}, com a cor de cabelo{cor_de_cabelo}, do tamanho {tamanho_cabelo},e estilo {estilo_cabelo}, estou cursando, {curso} e meu hobby é {hobby_fav}")
+print(f"Salve , meu nome é {nome}, tenho {idade} anos,{altura} de altura, meu genêro é {genero}, sou {cor_de_pele}, com a cor de cabelo{cor_de_cabelo}, do tamanho {tamanho_cabelo},e estilo {estilo_cabelo}, estou cursando, {curso} e meu hobby é {hobby_fav}")
 
 
